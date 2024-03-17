e48d78c18a590abf7ba1af44193bb0c311f3612e
adGit07 <142040762+adGit07@users.noreply.github.com>
2024-03-17 12:54:45 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,59 @@
+let questions =[
+    {
+        numb: 1,
+        question: "What does HTML stands for?",
+        answer: "A. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Text Markup Language",
+            "B. High Tech Multimedia Language",
+            "C. Home Tool Markup Language",
+            "D. Hyperlink and Text Markup Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which HTML attribute is used to specify an alternate text for an image?",
+        answer: "A. alt",
+        options: [
+            "A. alt",
+            "B. text",
+            "C. title",
+            "D. description"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which attribute is used to specify the URL of the page the link goes to?",
+        answer: "B. href",
+        options: [
+            "A. src",
+            "B. href",
+            "C. link",
+            "D. url"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does CSS stand for?",
+        answer: "C. Cascading Style Sheets",
+        options: [
+            "A. Computer Style Sheets",
+            "B. Creative Style Sheets",
+            "C. Cascading Style Sheets",
+            "D. Colorful Style Sheets"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which property is used in CSS to change the text color of an element?",
+        answer: "B. color",
+        options: [
+            "A. text-color",
+            "B. color",
+            "C. font-color",
+            "D. text-style"
+        ]
+    },
+
+]
+
